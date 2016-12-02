f219fa0bb1eb5ea016df02a76ad9c2d50b1e146f
yaoxuehua <xuehuayao@leedian.com>
2016-12-02 17:59:03 +0800

    增加背景动画效果

--- app/src/main/java/com/example/yaoxuehua/tourismattractionsapp/homepage/MainActivity.java
+++ app/src/main/java/com/example/yaoxuehua/tourismattractionsapp/homepage/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.yaoxuehua.tourismattractionsapp.homepage;
 
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.TextureMapView;
 import com.example.yaoxuehua.tourismattractionsapp.R;
 import com.example.yaoxuehua.tourismattractionsapp.homepage.FragmentAdapter.ViewPagerFragmentAdapter;
@@ -15,16 +14,42 @@ import com.example.yaoxuehua.tourismattractionsapp.homepage.fragment.MapNavigati
 import com.example.yaoxuehua.tourismattractionsapp.homepage.fragment.PersonalCenterFragment;
 import com.example.yaoxuehua.tourismattractionsapp.homepage.fragment.TourismRecommendFragment;
 import com.example.yaoxuehua.tourismattractionsapp.parent.activity.BaseTopBarActivity;
+import com.example.yaoxuehua.tourismattractionsapp.view.NoScrollViewPager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends BaseTopBarActivity {
+public class MainActivity extends BaseTopBarActivity implements View.OnClickListener {
 
-    private ViewPager viewPager;
+    private NoScrollViewPager viewPager;
     private LinearLayout viewGroup;
     private TextureMapView myMapView;
     private List<Fragment> fragmentList;
+    private RadioButton tourismBtn;
+    private RadioButton personalBtn;
+    private RadioButton mapBtn;
+    private int positionItem;//当前的选中选中页面
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.tourism_main_radio_btn:
+
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.personal_radio_btn_:
+
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.map_radio_btn:
+
+                viewPager.setCurrentItem(2);
+                break;
+        }
+    }
 
     @Override
     public void getSetContentViewBefore() {
@@ -44,14 +69,26 @@ public class MainActivity extends BaseTopBarActivity {
     protected void initView() {
         super.initView();
         setTitle("首页");
-        viewPager = (ViewPager) findViewById(R.id.Main_viewPager);
+        viewPager = (NoScrollViewPager) findViewById(R.id.Main_viewPager);
         viewGroup = (LinearLayout) findViewById(R.id.viewGroup);
-
-
+        tourismBtn = (RadioButton) findViewById(R.id.tourism_main_radio_btn);
+        personalBtn = (RadioButton) findViewById(R.id.personal_radio_btn_);
+        mapBtn = (RadioButton) findViewById(R.id.map_radio_btn);
         setFragmentList();
     }
 
-    public void setFragmentList(){
+    @Override
+    protected void initListener() {
+        super.initListener();
+        tourismBtn.setOnClickListener(this);
+        personalBtn.setOnClickListener(this);
+        mapBtn.setOnClickListener(this);
+    }
+
+    /**
+     * 设置三个碎片来切换
+     */
+    public void setFragmentList() {
         fragmentList = new ArrayList<>();
         TourismRecommendFragment tourismRecommendFragment = new TourismRecommendFragment();
         PersonalCenterFragment personalCenterFragment = new PersonalCenterFragment();
@@ -59,7 +96,9 @@ public class MainActivity extends BaseTopBarActivity {
         fragmentList.add(tourismRecommendFragment);
         fragmentList.add(personalCenterFragment);
         fragmentList.add(mapNavigationFragment);
-        ViewPagerFragmentAdapter viewPagerFragmentAdapter = new ViewPagerFragmentAdapter(getSupportFragmentManager(),fragmentList);
+        ViewPagerFragmentAdapter viewPagerFragmentAdapter = new ViewPagerFragmentAdapter(getSupportFragmentManager(), fragmentList);
         viewPager.setAdapter(viewPagerFragmentAdapter);
+        viewPager.setIfCanScorll(true);
     }
+
 }
